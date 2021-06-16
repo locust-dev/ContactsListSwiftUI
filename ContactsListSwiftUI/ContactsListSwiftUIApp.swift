a5cc832d053185acb3d90d5170739c46a3985c21
locust-dev <rrrr.ty30@gmail.com>
2021-06-16 22:09:43 +0300

    Initial Commit

---
+++ ContactsListSwiftUI/ContactsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListSwiftUIApp.swift
+//  ContactsListSwiftUI
+//
+//  Created by Илья Тюрин on 16.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
